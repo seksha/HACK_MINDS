5585801c608bf52dac6e89f1ddc8862c32a91e89
seksha <150233059+seksha@users.noreply.github.com>
2024-03-16 12:54:19 +0530

    Delete js directory

--- js/doctor.js
+++
@@ -1,7 +0,0 @@
-// Fetch appointments from doctor.php and append to appointments list
-fetch('doctor.php')
-  .then(response => response.text())
-  .then(data => {
-    document.getElementById('appointments-list').innerHTML = data;
-  })
-  .catch(error => console.error('Error fetching appointments:', error));
